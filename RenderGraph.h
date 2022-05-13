f1d7a328c0a2ca956cffdba8cd6fa1af02903659
zydiii <zydiiii@outlook.com>
2022-05-13 19:57:42 +0800

    implement render pass builder

--- RenderGraph.h
+++ RenderGraph.h
@@ -226,7 +226,6 @@ namespace RG {
 			stream << "}";
 		}
 
-
 	protected:
 		friend RG_renderpass_builder;
 
@@ -241,17 +240,4 @@ namespace RG {
 		std::vector<std::unique_ptr<RG_resource_base>> resources_; // 所有的资源
 		std::vector<step> timeline_; // 时间轴
 	};
-
-	template<typename resource_type, typename description_type>
-	resource_type* RG_renderpass_builder::create(const std::string& name, const description_type& description) {
-
-	}
-	template<typename resource_type>
-	resource_type* RG_renderpass_builder::read(resource_type* resource) {
-
-	}
-	template<typename resource_type>
-	resource_type* RG_renderpass_builder::write(resource_type* resource) {
-
-	}
 }
