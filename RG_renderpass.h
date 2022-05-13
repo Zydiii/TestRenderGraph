6f38c473eb54bfaaf89f9c7a3aeb3970fee32298
zydiii <zydiiii@outlook.com>
2022-05-13 15:51:17 +0800

    render pass

---
+++ RG_renderpass.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <functional>
+#include <string>
+
+#include "RG_renderpass_base.h"
+
+namespace RG {
+	class RG_renderpass_builder;
+
+	template<typename resource_type_>
+	class RG_renderpass : RG_renderpass_base {
+	public:
+		explicit RG_renderpass(const std::string& name, const std::function<void(resource_type_&, RG_renderpass_builder&)>& setup, const std::function<void(const resource_type_&)>& execute)
+			: RG_renderpass_base(name), setup_(setup), execute_(execute) {
+
+		}
+
+		virtual ~RG_renderpass() = default;
+
+		const resource_type_& data() const {
+			return resource_;
+		}
+
+	protected:
+		void setup(RG_renderpass_builder& builder) override {
+			setup_(resource_, builder);
+		}
+
+		void execute() const override {
+			execute_(resource_);
+		}
+
+		resource_type_ resource_; // 资源
+		const std::function<void(resource_type_&, RG_renderpass_builder&)> setup_; // 构建函数
+		const std::function<void(const resource_type_&)> execute_; // 执行函数
+	};
+}
